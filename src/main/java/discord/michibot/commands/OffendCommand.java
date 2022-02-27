f93fe85a1f37b43079b4d5055493cf4fda3bee8a
Andrés Osorio <gd_andres98@hotmail.com>
2022-02-27 15:26:45 -0500

    Offend slash command

---
+++ src/main/java/discord/michibot/commands/OffendCommand.java
@@ -0,0 +1,26 @@
+package discord.michibot.commands;
+
+import discord4j.core.event.domain.interaction.ChatInputInteractionEvent;
+import discord4j.core.object.command.ApplicationCommandInteractionOption;
+import discord4j.core.object.command.ApplicationCommandInteractionOptionValue;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+@Component
+public class OffendCommand implements SlashCommand {
+    @Override
+    public String getName() {
+        return "offend";
+    }
+
+    @Override
+    public Mono<Void> handle(ChatInputInteractionEvent event) {
+        String hijueputa = event.getOption("hijueputa")
+                .flatMap(ApplicationCommandInteractionOption::getValue)
+                .map(ApplicationCommandInteractionOptionValue::asString)
+                .get();
+
+        return event.reply()
+                .withContent(hijueputa + " es un perro hijueputa xd");
+    }
+}
