ec47b5afdb706fc46fb3a95be8134da7ed36dbf7
Andrés Osorio <gd_andres98@hotmail.com>
2022-02-27 13:26:36 -0500

    Inisital Commit

---
+++ src/main/java/discord/michibot/MichibotApplication.java
@@ -0,0 +1,13 @@
+package discord.michibot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MichibotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MichibotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/discord/michibot/MichibotApplicationTests.java
@@ -0,0 +1,13 @@
+package discord.michibot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MichibotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
