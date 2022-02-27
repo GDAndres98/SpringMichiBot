79d4b02721e7a5b35a829b84db595913656abaa7
Andrés Osorio <gd_andres98@hotmail.com>
2022-02-27 14:24:05 -0500

    DiscordBot Configuration

---
+++ src/main/java/discord/michibot/config/BotConfiguration.java
@@ -0,0 +1,26 @@
+package discord.michibot.config;
+
+import discord4j.core.DiscordClientBuilder;
+import discord4j.core.GatewayDiscordClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BotConfiguration {
+
+    @Value("${token}")
+    private String token;
+
+    @Bean
+    public GatewayDiscordClient gatewayDiscordClient() {
+        return DiscordClientBuilder.create(token)
+                .build()
+                .login()
+                .block();
+    }
+
+    public String getToken() {
+        return token;
+    }
+}
