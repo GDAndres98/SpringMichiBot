5ba166845eb96d3397cc2811b27b8daa767c715f
Andrés Osorio <gd_andres98@hotmail.com>
2022-02-27 15:14:43 -0500

    Comando Slash Ding

---
+++ src/main/java/discord/michibot/commands/DingCommand.java
@@ -0,0 +1,20 @@
+package discord.michibot.commands;
+
+import discord4j.core.event.domain.interaction.ChatInputInteractionEvent;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+@Component
+public class DingCommand implements SlashCommand {
+    @Override
+    public String getName() {
+        return "ding";
+    }
+
+    @Override
+    public Mono<Void> handle(ChatInputInteractionEvent event) {
+        return event.reply()
+//                .withEphemeral(true) para que solamente el command user vea el mensaje
+                .withContent("Dong!");
+    }
+}
