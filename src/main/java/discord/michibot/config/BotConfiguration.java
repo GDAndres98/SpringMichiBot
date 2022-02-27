c74b847b966330ba4aba946eba71f6183c172e5a
Andrés Osorio <gd_andres98@hotmail.com>
2022-02-27 15:13:32 -0500

    Configuracio servicio bot

--- src/main/java/discord/michibot/config/BotConfiguration.java
+++ src/main/java/discord/michibot/config/BotConfiguration.java
@@ -2,6 +2,9 @@ package discord.michibot.config;
 
 import discord4j.core.DiscordClientBuilder;
 import discord4j.core.GatewayDiscordClient;
+import discord4j.core.object.presence.ClientActivity;
+import discord4j.core.object.presence.ClientPresence;
+import discord4j.rest.RestClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,11 +19,14 @@ public class BotConfiguration {
     public GatewayDiscordClient gatewayDiscordClient() {
         return DiscordClientBuilder.create(token)
                 .build()
+                .gateway()
+                .setInitialPresence(ignore -> ClientPresence.online(ClientActivity.listening("tu corazón uwu")))
                 .login()
                 .block();
     }
 
-    public String getToken() {
-        return token;
+    @Bean
+    public RestClient discordRestClient(GatewayDiscordClient client) {
+        return client.getRestClient();
     }
 }
