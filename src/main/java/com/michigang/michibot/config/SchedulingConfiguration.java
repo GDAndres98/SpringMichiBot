05b8ea7d848bba7b49f02c64c5b84ae6fe0669df
Andres Osorio <gd_andres98@hotmail.com>
2022-05-03 00:38:20 -0500

    SchedulingService to prevent Heroku's sleep mode

---
+++ src/main/java/com/michigang/michibot/config/SchedulingConfiguration.java
@@ -0,0 +1,13 @@
+package com.michigang.michibot.config;
+
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+@ConditionalOnProperty(name = "scheduling.enable", matchIfMissing = true)
+public class SchedulingConfiguration {
+
+}
---
+++ src/main/java/com/michigang/michibot/service/SchedulingService.java
@@ -0,0 +1,34 @@
+package com.michigang.michibot.service;
+
+import discord4j.core.GatewayDiscordClient;
+import discord4j.core.object.presence.ClientActivity;
+import discord4j.core.object.presence.ClientPresence;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SchedulingService {
+
+    private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    GatewayDiscordClient gatewayDiscordClient;
+
+    // Prevent Heroku to set bot in sleep mode
+    @Scheduled(fixedDelay = 300000L)
+    private void pingSelf() {
+        try {
+            boolean dateParityEven = (System.currentTimeMillis() / 60000) % 2 == 0;
+            LOGGER.info("El michi respira: " + String.valueOf(dateParityEven));
+            gatewayDiscordClient.updatePresence(
+                    ClientPresence.online(ClientActivity.listening(dateParityEven ? "Ding" : "Dong"))).block();
+        } catch (Exception e) {
+            LOGGER.error("EL MICHI NO RESPIRA!");
+        }
+    }
+
+
+}
