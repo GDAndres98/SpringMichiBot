7f7de97eaf22de4e471b03ece2561dc8b3557d14
Andrés Gustavo <gd_andres98@hotmail.com>
2022-09-01 20:08:16 -0500

    Bot Token app variable name

--- src/main/java/com/michigang/michibot/config/BotConfiguration.java
+++ src/main/java/com/michigang/michibot/config/BotConfiguration.java
@@ -12,7 +12,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class BotConfiguration {
 
-    @Value("${token}")
+    @Value("${bot.token}")
     private String token;
 
     @Bean
